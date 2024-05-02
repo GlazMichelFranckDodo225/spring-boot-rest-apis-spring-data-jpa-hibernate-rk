a111a96dff41f7acb8ab08f79956e1a152ffb456
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-05-02 06:16:54 +0200

    Enhancing the Hello World REST API with a Path Variable

--- src/main/java/com/dgmf/controller/HelloWorldController.java
+++ src/main/java/com/dgmf/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.dgmf.controller;
 
 import com.dgmf.entity.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +18,11 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello world !");
+    }
+
+    // Hello world Path Variable REST API
+    @GetMapping("/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable("name") String name) {
+        return new HelloWorldBean(String.format("Hello %s", name));
     }
 }
