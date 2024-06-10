90b2e03eecce2eeddc556949bee886702b3b6df1
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-06-10 08:36:54 +0200

    Internationalization (i18n) for REST API

--- src/main/java/com/dgmf/controller/HelloWorldController.java
+++ src/main/java/com/dgmf/controller/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.dgmf.controller;
 
 import com.dgmf.entity.HelloWorldBean;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
+@RequiredArgsConstructor
 public class HelloWorldController {
+    private final MessageSource messageSource;
+
     // Hello world REST API
     // @RequestMapping(path = "/hello-world", method = RequestMethod.GET)
     @GetMapping("/hello-world")
@@ -24,5 +32,27 @@ public class HelloWorldController {
     @GetMapping("/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable("name") String name) {
         return new HelloWorldBean(String.format("Hello %s", name));
+    }
+
+    // Hello world i18n REST API
+    /*
+    English : 'en'
+    Dutch : 'nl'
+    French : 'fr'
+    Deutsch : 'de'
+    */
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        // Get Locale from Request Header
+        Locale locale = LocaleContextHolder.getLocale();
+
+        return messageSource.getMessage(
+                "good.morning.message",
+                null,
+                "Default Message",
+                locale
+        );
+
+        // return "Hello world i18n!";
     }
 }
