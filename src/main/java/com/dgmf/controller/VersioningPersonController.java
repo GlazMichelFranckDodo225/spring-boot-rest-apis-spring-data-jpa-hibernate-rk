7976905158bab88b92169e6235d83677d64ded11
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-06-11 06:03:39 +0200

    Versioning REST API - Request Param

--- src/main/java/com/dgmf/controller/VersioningPersonController.java
+++ src/main/java/com/dgmf/controller/VersioningPersonController.java
@@ -17,4 +17,14 @@ public class VersioningPersonController {
     public PersonV2 getSecondVersionOfPerson() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
+
+    @GetMapping(path = "/persons", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/persons", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParameter() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
 }
