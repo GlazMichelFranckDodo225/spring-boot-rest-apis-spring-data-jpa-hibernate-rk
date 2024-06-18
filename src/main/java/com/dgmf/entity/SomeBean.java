d8e2a0ccd0e0cd945a404cebb91ab6f6daff5bc0
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-06-18 13:16:45 +0200

    Static Filtering : @JsonIgnoreProperties() on Class Level

--- src/main/java/com/dgmf/entity/SomeBean.java
+++ src/main/java/com/dgmf/entity/SomeBean.java
@@ -1,15 +1,17 @@
 package com.dgmf.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
+// @JsonIgnoreProperties(value = {"field1", "field2"})
+@JsonIgnoreProperties({"field1", "field2"})
 public class SomeBean {
     private String field1;
-    @JsonIgnore
+    // @JsonIgnore
     private String field2;
     private String field3;
 }
