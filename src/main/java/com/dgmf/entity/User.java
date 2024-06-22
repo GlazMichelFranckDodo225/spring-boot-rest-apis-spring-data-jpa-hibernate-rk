0b39be002d2ab8f470421750e828ce955771e13c
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-06-22 06:30:07 +0200

    Update User Entity

--- src/main/java/com/dgmf/entity/User.java
+++ src/main/java/com/dgmf/entity/User.java
@@ -1,5 +1,8 @@
 package com.dgmf.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -10,7 +13,9 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
+@Entity(name = "user_details")
 public class User {
+    @Id @GeneratedValue
     private Long id;
     @Size(min = 2, message = "Name Should Have at Least 2 Characters")
     private String name;
