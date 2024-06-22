f4a4bca3344c02cd1faac55e20b0748c0b8191fb
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-06-22 06:53:06 +0200

    added "data.sql" and Updated "application.properties" File

---
+++ src/main/resources/data.sql
@@ -0,0 +1,27 @@
+INSERT INTO user_details(
+    id,
+    birth_date,
+    name
+) VALUES(
+    10001,
+    current_date(),
+    'Ranga'
+);
+INSERT INTO user_details(
+    id,
+    birth_date,
+    name
+) VALUES(
+    10002,
+    current_date(),
+    'Ravi'
+);
+INSERT INTO user_details(
+    id,
+    birth_date,
+    name
+) VALUES(
+    10003,
+    current_date(),
+    'Sathish'
+);
